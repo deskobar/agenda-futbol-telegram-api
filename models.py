5b359ffa892ed775138b81a76dc4acaf9f359206
David Escobar <david.escobar@ug.uchile.cl>
2024-09-30 00:23:53 -0300

    Refactor models.py and pyproject.toml

--- models.py
+++ models.py
@@ -8,16 +8,14 @@ database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
 
-class BaseMeta(ormar.ModelMeta):
-    metadata = metadata
-    database = database
-
-
 class Alias(ormar.Model):
-    class Meta(BaseMeta):
-        tablename = "alias"
+    ormar_config = ormar.OrmarConfig(
+        database=database,
+        metadata=metadata,
+        tablename="alias",
+    )
 
-    id: int = ormar.Integer(primary_key=True)
+    id: int = ormar.Integer(primary_key=True, autoincrement=True)
     user_id: str = ormar.String(max_length=100)
     team_name: str = ormar.String(max_length=100)
     alias: str = ormar.String(max_length=100)
